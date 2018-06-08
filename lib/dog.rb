23d4837818aa14bb4f2567de7f08d2afec0d9f29
bayleighanne <bayhaf718@gmail.com>
2018-06-08 16:49:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,11 @@
 class Dog
-  def name = (dog_name)
-    "this_dogs_name" = "fido"
+  def name=(dog_name)
+    "this_dogs_name"="fido"
   end 
   
   def name 
     "this_dogs_name"
   end 
-end 
 
 fido = Dog.new 
 fido.name = "fido"
